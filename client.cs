d92378fc1aa107decc58d7e8cf873704317523bd
JeffreyI <36392018+JeffreyI@users.noreply.github.com>
2018-04-10 11:33:50 -0700

    Add files via upload

---
+++ client.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace _471_FTP_Client
+{
+    class ftp
+    {
+        public TcpClient control = new TcpClient(); //client is on port 2221
+        public int bufferSize = 500;
+
+        public void sendFile(string fileName)
+        {
+            TcpClient data = new TcpClient();
+            IPAddress hostIPAddress1 = IPAddress.Parse("127.0.0.1");
+            var port = 2222;
+            if (control == null)
+            {
+                control = new TcpClient(hostIPAddress1.ToString(), port);
+            }
+            var endp = new IPEndPoint(hostIPAddress1, port);
+            data.Connect(endp);
+            
+            byte[] bytebuffer = new byte[bufferSize];
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop))
+                                        + "\\" + fileName;
+            if (File.Exists(path))
+            {
+                try
+                {
+                    Stream localFile = File.OpenRead(path);
+                    int byteSent = localFile.Read(bytebuffer, 0, bufferSize);
+                    int fileSize = 0;
+
+                    NetworkStream fileStream = data.GetStream();
+                    while (byteSent != 0)
+                    {
+                        fileSize += byteSent;
+                        fileStream.Write(bytebuffer, 0, byteSent);
+                        byteSent = localFile.Read(bytebuffer, 0, bufferSize);
+                    }
+                    localFile.Close();
+                    fileStream.Close();
+                    Console.WriteLine("File Sent: " + fileName + " " + fileSize);
+                }
+                catch(IOException ie)
+                {
+                    Console.WriteLine("IO Error: " + ie.Message);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("General Error: " + e.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Path Does Not Exist!");
+            }
+            data.Close();
+        }
+
+        public void getFile(string fileName)
+        {
+            TcpClient data = new TcpClient();
+            IPAddress hostIPAddress1 = IPAddress.Parse("127.0.0.1");
+            var port = 2222;
+            if (control == null)
+            {
+                control = new TcpClient(hostIPAddress1.ToString(), port);
+            }
+            var endp = new IPEndPoint(hostIPAddress1, port);
+            data.Connect(endp);
+
+            byte[] bytebuffer = new byte[bufferSize];
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop)) + "\\FTP_Server_Dir\\" + fileName;
+            string path_desk = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop))
+                                + "\\" + fileName;
+
+            if (File.Exists(path))
+            {
+                try
+                {
+
+                    NetworkStream fileStream = data.GetStream();
+                    FileStream localFile = File.Open(path_desk, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
+                    int byteRecv;
+                    int fileSize = 0;
+                    while ((byteRecv = fileStream.Read(bytebuffer, 0, bufferSize)) > 0)
+                    {
+                        fileSize += byteRecv;
+                        if (byteRecv == 0) break;
+                        localFile.Write(bytebuffer, 0, byteRecv);
+
+                    }
+                    Console.WriteLine("File Received: " + fileName + " " + fileSize);
+                    fileStream.Close();
+                    localFile.Close();
+                }
+                catch (IOException ie)
+                {
+                    Console.WriteLine("IO Error: " + ie.Message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("General Error: " + e.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Path Does Not Exist!");
+            }
+            data.Close();
+        }
+
+        public void listLs()
+        {
+            TcpClient data = new TcpClient();
+            IPAddress hostIPAddress1 = IPAddress.Parse("127.0.0.1");
+            var port = 2222;
+            if (control == null)
+            {
+                control = new TcpClient(hostIPAddress1.ToString(), port);
+            }
+            var endp = new IPEndPoint(hostIPAddress1, port);
+            data.Connect(endp);
+
+            byte[] bytebuffer = new byte[bufferSize];
+            NetworkStream fileStream = data.GetStream();
+            int byteRecv;
+            string dataLs = null;
+            byte[] msg = null;
+            while ((byteRecv = fileStream.Read(bytebuffer, 0, bufferSize)) != 0)
+            {
+                dataLs = System.Text.Encoding.ASCII.GetString(bytebuffer, 0, byteRecv);
+                Console.WriteLine("List Received: \t{0}", dataLs);
+                msg = System.Text.Encoding.ASCII.GetBytes(dataLs);
+            }
+            fileStream.Close();
+            data.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            ftp data = new ftp();
+            TcpClient control = new TcpClient();
+            IPAddress hostIPAddress1 = IPAddress.Parse("127.0.0.1");
+            var port = 2221;
+            if (control == null)
+            {
+                control = new TcpClient(hostIPAddress1.ToString(), port);
+            }
+            var endp = new IPEndPoint(hostIPAddress1, port);
+            control.Connect(endp);
+
+            Console.WriteLine("Creating a Server Directory on Desktop!: FTP_Server_Dir");
+            string path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "FTP_Server_Dir");
+            if(!Directory.Exists(path))
+            {
+                try
+                {
+                    Directory.CreateDirectory(path);
+                }
+                catch (IOException ie)
+                {
+                    Console.WriteLine("IO Error: " + ie.Message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("General Error: " + e.Message);
+                }
+            }
+
+            string opt = null;
+            string fileName = null;
+            bool done = false;
+            Console.WriteLine("Commands: put, get, ls and quit ");
+            while (!done)
+            {
+                Console.Write("ftp> ");
+                string[] str = Console.ReadLine().Split(new[] { ' '},2);
+                opt = str[0];
+                if (str.Length > 1)
+                {
+                    fileName = str[1];
+                    opt = str[0] + " " + str[1];
+                }
+                else
+                {
+                    opt = str[0];
+                }
+                if (str[0].CompareTo("put") == 0)
+                {
+                    byte[] put = Encoding.ASCII.GetBytes(opt);
+                    NetworkStream nwStream = control.GetStream();
+                    Console.WriteLine("Sending: " + Encoding.ASCII.GetString(put, 0, put.Length));
+                    nwStream.Write(put, 0, put.Length);
+
+                    data.sendFile(fileName);
+                }
+                else if (str[0].CompareTo("get") == 0)
+                {
+                    byte[] get = Encoding.ASCII.GetBytes(opt);
+                    NetworkStream nwStream = control.GetStream();
+                    Console.WriteLine("Sending: " + Encoding.ASCII.GetString(get, 0, get.Length));
+                    nwStream.Write(get, 0, get.Length);
+
+                    data.getFile(fileName);
+                }
+                else if (str[0].CompareTo("ls") == 0)
+                {
+                    byte[] get = Encoding.ASCII.GetBytes(opt);
+                    NetworkStream nwStream = control.GetStream();
+                    Console.WriteLine("Sending: " + Encoding.ASCII.GetString(get, 0, get.Length));
+                    nwStream.Write(get, 0, get.Length);
+
+                    data.listLs();
+                }
+                else if (str[0].CompareTo("quit") == 0)
+                {
+                    done = true;
+
+                    byte[] get = Encoding.ASCII.GetBytes(opt);
+                    NetworkStream nwStream = control.GetStream();
+                    Console.WriteLine("Sending: " + Encoding.ASCII.GetString(get, 0, get.Length));
+                    nwStream.Write(get, 0, get.Length);
+
+                    control.Close();
+                }
+                else
+                {
+                    Console.WriteLine("No command matched!");
+                }
+            }
+            control.Close();
+            Console.WriteLine("Client closed");
+        }
+    }
+}
---
+++ server.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections;
+
+class Server
+{
+    public int bufferSize = 500;
+    public void put(string fileName)
+    {
+        TcpListener dataServer = null;
+        // Set the TcpListener on port 2221.
+        Int32 port = 2222;
+        IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+        // TcpListener server = new TcpListener(port);
+        dataServer = new TcpListener(localAddr, port);
+
+        // Start listening for client requests.
+        dataServer.Start();
+        Console.Write("Waiting for a connection... ");
+        TcpClient server = dataServer.AcceptTcpClient();
+        Console.WriteLine("Connected!");
+
+        byte[] bytebuffer = new byte[bufferSize];
+
+        string path_desk = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop))
+                                        + "\\" + fileName;
+
+        if (File.Exists(path_desk))
+        {
+            try
+            {
+                string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop))
+                                              + "\\FTP_Server_Dir\\" + fileName;
+                Stream localFile = File.Open(path, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
+                NetworkStream fileStream = server.GetStream();
+
+                int byteRecv;
+                int fileSize = 0;
+                while ((byteRecv = fileStream.Read(bytebuffer, 0, bufferSize)) > 0)
+                {
+                    fileSize += byteRecv;
+                    if (byteRecv == 0) break;
+                    localFile.Write(bytebuffer, 0, byteRecv);
+
+                }
+                Console.WriteLine("File Received: " + fileName + " " + fileSize);
+                fileStream.Close();
+                localFile.Close();
+            }
+            catch (IOException ie)
+            {
+                Console.WriteLine("IO Error: " + ie.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("General Error: " + e.Message);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Path Does Not Exists!");
+        }
+        dataServer.Stop();
+        server.Close();
+    }
+
+    public void get(string fileName)
+    {
+        TcpListener dataServer = null;
+        // Set the TcpListener on port 2221.
+        Int32 port = 2222;
+        IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+        // TcpListener server = new TcpListener(port);
+        dataServer = new TcpListener(localAddr, port);
+
+        // Start listening for client requests.
+        dataServer.Start();
+        Console.Write("Waiting for a connection... ");
+        TcpClient server = dataServer.AcceptTcpClient();
+        Console.WriteLine("Connected!");
+
+        string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop))
+                                        + "\\FTP_Server_Dir\\" + fileName;
+        if (File.Exists(path))
+        {
+            try
+            {
+                Stream localFile = File.OpenRead(path);
+                byte[] bytebuffer = new byte[bufferSize];
+                int byteSent = localFile.Read(bytebuffer, 0, bufferSize);
+                int fileSize = 0;
+
+                NetworkStream fileStream = server.GetStream();
+                while (byteSent != 0)
+                {
+                    fileSize += byteSent;
+                    fileStream.Write(bytebuffer, 0, byteSent);
+                    byteSent = localFile.Read(bytebuffer, 0, bufferSize);
+                }
+
+                Console.WriteLine("File Sent: " + fileName + " " + fileSize);
+                fileStream.Close();
+                localFile.Close();
+            }
+            catch (IOException ie)
+            {
+                Console.WriteLine("IO Error: " + ie.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("General Error: " + e.Message);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Path Does Not Exists!");
+        }
+        dataServer.Stop();
+        server.Close();
+    }
+
+    public static void ProcessFile(string path)
+    {
+        Console.WriteLine("Processed file '{0}'.", path);
+    }
+    public void ls()
+    {
+        TcpListener dataServer = null;
+        // Set the TcpListener on port 2221.
+        Int32 port = 2222;
+        IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+        // TcpListener server = new TcpListener(port);
+        dataServer = new TcpListener(localAddr, port);
+
+        // Start listening for client requests.
+        dataServer.Start();
+        Console.Write("Waiting for a connection... ");
+        TcpClient server = dataServer.AcceptTcpClient();
+        Console.WriteLine("Connected!");
+
+        string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop))
+                                        + "\\FTP_Server_Dir\\";
+        if (Directory.Exists(path))
+        {
+            try
+            {
+                //create a list of items in the Directory
+                string[] fileEntries = Directory.GetFiles(path);
+                string fileName = null;
+                for (int i = 0; i < fileEntries.Length; i++)
+                {
+                    fileName += Path.GetFileName(fileEntries[i]) + " " +'\t';
+                }
+
+                byte[] ls = Encoding.ASCII.GetBytes(fileName);
+                NetworkStream nwStream = server.GetStream();
+                Console.WriteLine("Sending: \t" + Encoding.ASCII.GetString(ls, 0, ls.Length));
+                nwStream.Write(ls, 0, ls.Length);
+
+            }
+            catch (IOException ie)
+            {
+                Console.WriteLine("IO Error: " + ie.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("General Error: " + e.Message);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Path Does Not Exists!");
+        }
+        dataServer.Stop();
+        server.Close();
+    }
+
+    public static void Main()
+    {
+        Server tcpServer = new Server();
+        TcpListener server = null;
+        try
+        {
+            // Set the TcpListener on port 2221.
+            Int32 port = 2221;
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+            // TcpListener server = new TcpListener(port);
+            server = new TcpListener(localAddr, port);
+            // Start listening for client requests.
+            server.Start();
+
+            // Buffer for reading data
+            Byte[] bytes = new Byte[256];
+            String data = null;
+
+            // Enter the listening loop.
+            while (true)
+            {
+                Console.Write("Waiting for a connection... ");
+
+                // Perform a blocking call to accept requests.
+                // You could also user server.AcceptSocket() here.
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Connected!");
+
+                data = null;
+
+                // Get a stream object for reading and writing
+                NetworkStream stream = client.GetStream();
+
+                int i;
+
+                // Loop to receive all the data sent by the client.
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    // Translate data bytes to a ASCII string.
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine("Received: {0}", data);
+
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);//.Split(new[] { ' '},2));
+                    
+                    string[] str = Encoding.UTF8.GetString(msg, 0, msg.Length).Split(new[] { ' '},2);
+
+                    if (str[0].CompareTo("put") == 0)
+                    {
+                        //put function
+                        tcpServer.put(str[1]);
+                    }
+                    else if (str[0].CompareTo("get") == 0)
+                    {
+                        //get function
+                        tcpServer.get(str[1]);
+                    }
+                    else if (str[0].CompareTo("ls") == 0)
+                    {
+                        //ls function
+                        tcpServer.ls();
+                    }
+                    else if(str[0].CompareTo("quit") == 0)
+                    {
+                        //quit
+                        server.Stop();
+                        break;
+                    }
+                }
+                // Shutdown and end connection
+                client.Close();
+                break;
+            }
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine("SocketException: {0}", e);
+        }
+        finally
+        {
+            // Stop listening for new clients.
+            server.Stop();
+        }
+        
+        Console.WriteLine("\nHit enter to continue...");
+        Console.Read();
+    }
+}
